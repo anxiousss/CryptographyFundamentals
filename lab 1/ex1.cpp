7183b53a027a1299ff3f5dbe97ae5552cdcb008a
anxiousss <saimonkheresh@gmail.com>
2025-10-04 20:48:13 +0300

    function refactoring

--- lab 1/ex1.cpp
+++ lab 1/ex1.cpp
@@ -25,25 +25,21 @@ void set_younger_bit(std::byte& b, size_t n, bool value) {
     b = (b & ~(std::byte(1) << n)) | (std::byte(value) << n);
 }
 
-std::vector<std::byte> bits_permutation(std::vector<std::byte>& msg, const std::vector<unsigned int>& IP,
+std::vector<std::byte> bits_permutation(std::vector<std::byte> msg, const std::vector<unsigned int>& IP,
                         bool indexing_rule, bool staring_bit_number) {
 
-    // если starting_bit_number = true, то вычитаем везде 1 из позиции
     size_t n_msg = msg.size();
     size_t bits_number = n_msg * 8;
-    std::vector<std::byte> permutation{n_msg, std::byte{0}} ;
-    if (indexing_rule) {
-        for (size_t i = 0; i < bits_number; ++i) {
-            auto& b = permutation[i / 8];
-            set_eldest_bit(b, i % 8,
-                           get_eldest_bit(msg[IP[i] / 8], (IP[i] % 8) - staring_bit_number));
-        }
-    } else {
-        for (size_t i = 0; i < bits_number; ++i) {
-            auto& b = permutation[i / 8];
-            std::cout << permutation[0] << ' ' << permutation[1] << std::endl;
-            set_younger_bit(b, 7 - i % 8 ,
-                           get_younger_bit(msg[(bits_number - IP[i]) / 8], IP[i] % 8 - 1));
+    std::vector<std::byte> permutation{n_msg, std::byte{0}};
+    for (int i = 0; i < bits_number; ++i) {
+        auto& b = permutation[i / 8];
+        if (indexing_rule) {
+            auto value = get_eldest_bit(msg[IP[i] / 8], (IP[i] % 8) - staring_bit_number);
+            set_eldest_bit(b, i % 8, value);
+        } else {
+            // std::cout << permutation[0] << ' '  << permutation[1] << std::endl;
+            auto value = get_younger_bit(msg[(bits_number - IP[i]) / 8], IP[i] % 8 - staring_bit_number);
+            set_younger_bit(b, 7 - i % 8, value);
         }
     }
 
@@ -58,11 +54,14 @@ int main() {
 
     std::cout << msg[0] << ' ' << msg[1] << std::endl;
 
-    std::vector<unsigned int> IP = {15, 14, 2, 7, 6, 1, 3, 10, 11, 9, 4, 5, 8, 13, 12, 16};
+    std::vector<unsigned int> IP_1 = {15, 14, 2, 7, 6, 1, 3, 10, 11, 9, 4, 5, 8, 13, 12, 16};
+    std::vector<unsigned int> IP_2 = {14, 13, 1, 6, 5, 0, 2, 9, 10, 8, 3, 4, 7, 12, 11, 15};
     bool rule = false, bit_number = true;
 
-    auto P = bits_permutation(msg, IP, rule, bit_number);
-    std::cout << P[0] << ' ' << P[1];
+    auto P_1 = bits_permutation(msg, IP_1, rule, bit_number);
+    std::cout << P_1[0] << ' ' << P_1[1] << std::endl;
+    auto P_2 = bits_permutation(msg, IP_2, rule, !bit_number);
+    std::cout << P_2[0] << ' ' << P_2[1] << std::endl;
 
     return 0;
 }
